c81104814761cf99eb088a81580f9abef869633e
Muyang liu <liumuyang040431@gmail.comgit config --global user.name Muyang>
2017-03-15 16:39:45 +1100

    do exercise1

---
+++ fixer.sh
@@ -0,0 +1,30 @@
+# edit these lines
+MYNAME="Ben Doherty"
+MYEMAIL="ben@notionparallax.co.uk"
+# stop editing
+
+
+# don't edit these lines
+#git config --global user.name $MYNAME
+#git config --global user.email $MYEMAIL
+#git config --global credential.helper 'cache --timeout=36000' #cache password for 150 minutes
+#git config --global color.ui auto #colour the output in git
+#git config --global core.editor "atom --wait"
+#
+##in case this sh has been run multiple times before
+#git config --global --replace-all user.email $MYEMAIL
+#git config --global --replace-all user.name $MYNAME
+
+apm install script
+apm install linter
+apm install linter-flake8
+
+sudo pip install colorama
+
+sudo pip uninstall flake8 -y
+sudo pip install flake8==3.2.0
+
+echo
+echo "That should have fixed the bugs"
+echo
+git config --list
